0424f61300ffa4cac6de74f76e45bbc0d9b5b8e7
Fedir <fedir.sinchuk@outlook.com>
2019-07-16 15:29:24 +0300

    Init

---
+++ TeamCityOld/TC.OLD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TC.OLD.Startup))]
+namespace TC.OLD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
